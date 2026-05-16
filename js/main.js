2ce4e7c7aeddecccca1057e9503f52b66e5c53a3
andelipe.95-stack <andelipe.95@gmail.com>
2026-05-16 20:00:01 +0200

    added localStorage with toggle

--- js/main.js
+++ js/main.js
@@ -22,13 +22,32 @@ searchInput.addEventListener("input", handleSearch);
 
 const themeToggle = document.getElementById("themeToggle");
 
+const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "light") {
+        document.body.classList.add("light-mode");
+
+        themeToggle.textContent = "☀️";
+    }
+
 themeToggle.addEventListener("click", () =>{
+
+    
 
     document.body.classList.toggle("light-mode");
 
     if(document.body.classList.contains("light-mode")){
         themeToggle.textContent = "☀️";
+
+        localStorage.setItem("theme", "light");
+
     }else{
+
         themeToggle.textContent = "🌙";
+
+        localStorage.setItem("theme", "dark");
     }
+
+    
+
 });
